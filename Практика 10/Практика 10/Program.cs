dc1a9d7bdf0abbad0f3f0536afd67d5e49a419c1
ACER <ACER@DESKTOP-O7A416V>
2022-11-14 13:42:21 +0300

    Добавьте файлы проекта.

---
+++ Практика 10/Практика 10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double z, x, n;
+            z = 0;
+            Console.Write("x = ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("(n>0) n = ");
+            n = double.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++) z += -((1 / (2 * i - 1)) * Math.Pow(((x - 1) / (x + 1)), 2 * i - 1));
+            Console.WriteLine("z = {0}", z);
+        }
+    }
+}
